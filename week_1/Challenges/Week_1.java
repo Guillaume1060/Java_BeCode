5f1d7ebf6b1621036da0359cd6c7ae3e176cb0d2
Guillaume <breyerguillaume@gmail.com>
2023-08-06 15:50:48 +0200

    initFirstWk

--- week_1/Challenges/Week_1.java
+++ week_1/Challenges/Week_1.java
@@ -21,6 +21,10 @@ public class Week_1 {
     boolean aBoolean = true;
 
     // Challenge 2
-
+//    These are objects. Primitive data types are raw values, but objects are not.
+//    Objects are actually pointers, they point to memory in the jvm.
+//    So a variable of type Integer can also NOT have a pointer, which makes it 'null'.
+//    Very extra:
+//    Is there a way to loop over all of the variables automatically and only have 1 print statement? If so, why was this not possible the primitive datatypes?
 
 }
