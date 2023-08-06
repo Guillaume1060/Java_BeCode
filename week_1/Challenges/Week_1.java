d7d07a2fb44d6ff02c404701d733af77d07b79ae
Guillaume <breyerguillaume@gmail.com>
2023-08-06 16:10:23 +0200

    initFirstWk

--- week_1/Challenges/Week_1.java
+++ week_1/Challenges/Week_1.java
@@ -11,20 +11,21 @@ public class Week_1 {
 */
 
     // Challenge 1
-    byte aByte = 127;
-    short aShort = 32_767;
-    int anInt = 2_147_483_646;
-    long aLong = 9_223_372_036_854_775_807L;
-    float aFloat = 7.55f;
-    double aDouble = 5.55;
-    char aChar = 'Z';
-    boolean aBoolean = true;
+//    byte aByte = 127;
+//    short aShort = 32_767;
+//    int anInt = 2_147_483_646;
+//    long aLong = 9_223_372_036_854_775_807L;
+//    float aFloat = 7.55f;
+//    double aDouble = 5.55;
+//    char aChar = 'Z';
+//    boolean aBoolean = true;
 
-    // Challenge 2
-//    These are objects. Primitive data types are raw values, but objects are not.
-//    Objects are actually pointers, they point to memory in the jvm.
-//    So a variable of type Integer can also NOT have a pointer, which makes it 'null'.
-//    Very extra:
-//    Is there a way to loop over all of the variables automatically and only have 1 print statement? If so, why was this not possible the primitive datatypes?
+//    CHALLENGE 4
+//    Write a small program that will build up a random array of non-primitive integers, and 0 is a valid entry.
+//    Now loop over the array and divide each integer with the previous integer.
+//    The first integer can be divided by itself.
+//    How will you make sure that the program doesn't crash when dividing by zero?
+//    This challenge can be a way to introduce exceptions, try catch blocks, etc. Extra
+
 
 }
