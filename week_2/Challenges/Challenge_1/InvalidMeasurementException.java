359ce4ecfdaed7cb332ea902c86782c4dbd8d171
Guillaume <breyerguillaume@gmail.com>
2023-08-19 16:00:31 +0200

    EndChallenge_1

---
+++ week_2/Challenges/Challenge_1/InvalidMeasurementException.java
@@ -0,0 +1,7 @@
+package week_2.Challenges.Challenge_1;
+
+public class InvalidMeasurementException extends Exception {
+    public InvalidMeasurementException(String message) {
+        super(message);
+    }
+}
