ae5118216f17aa1c764c70d82140fcea138f6b9d
Guillaume <breyerguillaume@gmail.com>
2023-08-30 18:33:50 +0200

    EndChallenge_1

---
+++ week_2/Challenges/Challenge_2/Main.java
@@ -0,0 +1,29 @@
+package week_2.Challenges.Challenge_2;
+
+public class Main {
+    public static void main(String[] args) {
+        String csvFilePath = "week_2/Challenges/Challenge_2/covid_and_trade.csv";
+        CSVReader csvReader = new CSVReader(csvFilePath);
+        // Making dataList
+//        List<DataModel> dataModelSet = csvReader.dataModelSet;
+
+        // Challenge 2. Method to print unique values per column
+//         csvReader.printUniqueValues();
+
+
+        // Challenge 3. Filtering by year and country and rejecting re-imports values
+//        csvReader.printSpecificValues(2016,"China");
+
+        // Challenge 4. More filtering and even more transforming
+//        csvReader.printValuesInEuro(0.85);
+
+        // Challenge 5. Filtering and sorting
+//        csvReader.printSortedValues(2018);
+
+        // Challenge 6. Finding the highest value of
+//        csvReader.printHighestValue();
+
+        // Challenge 7.
+
+    }
+}
---
+++ week_2/Challenges/Challenge_5/Challenge_5.md
@@ -0,0 +1,16 @@
+# Challenge 5: Filtering and sorting
+
+- Repository: `java_week_2`
+
+- Type of Challenge: `Learning`
+
+- Team challenge : `solo`
+
+  
+
+## The Mission
+
+Using the same csv file, write a program that prints out a sorted list of the import values of the year 2018.
+
+The country, commodity and transport_mode need to have the value 'all' when you filter. The output
+needs to be sorted from smallest to biggest (ascending). 
