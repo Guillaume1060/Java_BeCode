2f761519e71c1b85b3a8dd5dfbb04258e1d836c3
Guillaume <breyerguillaume@gmail.com>
2023-08-31 12:06:29 +0200

    refactor files

--- week_2/Challenges/Challenge_1/InvalidMeasurementException.java
+++
@@ -1,7 +0,0 @@
-package week_2.Challenges.Challenge_1;
-
-public class InvalidMeasurementException extends Exception {
-    public InvalidMeasurementException(String message) {
-        super(message);
-    }
-}
